1a1b3eb0c1e0c2a3a61ad51df0da0263b9cb638e
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-10-14 17:58:06 +0530

    [master] Added a new set of code for Lights API.

--- api/v1/controllers/lights_controller.go
+++ api/v1/controllers/lights_controller.go
@@ -15,13 +15,41 @@ var Lights LightsController
 
 func (l *LightsController) Toggle_led_light(rw http.ResponseWriter, req *http.Request) {
 	flag.Parse()
-
 	embd.InitGPIO()
-	defer embd.CloseGPIO()
 
-	embd.SetDirection(17, embd.Out)
-	embd.DigitalWrite(17, embd.High)
-	time.Sleep(1 * time.Second)
-	embd.SetDirection(17, embd.Out)
-	embd.DigitalWrite(17, embd.Low)
+	body, err := ioutil.ReadAll(req.Body)
+	flag := 1
+	var lig models.Light
+
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(body, &lig)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(lig.Pin_number)
+	if lig.Pin_number == 17 {
+		embd.SetDirection(lig.Pin_number, embd.Out)
+		embd.DigitalWrite(lig.Pin_number, embd.High)
+		b, err := json.Marshal(models.LightMessage{
+			Success: "True",
+			Message: "Light blinked",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write(b)
+	} else {
+		b, err := json.Marshal(models.LightMessage{
+			Success: "false",
+			Message: "Light did not blink",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write(b)
+	}
 }
---
+++ api/v1/models/light.go
@@ -0,0 +1,10 @@
+package models
+
+type Light struct {
+	Pin_number int
+}
+
+type LightMessage struct {
+	Success string
+	Message string
+}
