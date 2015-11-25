bb43c1f0661d9cc73926f78545f3366ee54f77a0
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2015-11-25 19:32:06 +0530

    [master ircontrol] Fixes.

--- api/v1/controllers/weather_controller.go
+++ api/v1/controllers/weather_controller.go
@@ -133,37 +133,37 @@ func (t *TemperatureController) Monitor_temperature_humidity(rw http.ResponseWri
 	embd.InitGPIO()
 	// var lig models.Light
 
-	// sensorType := dht.DHT11
-	// temperature, humidity, retried, err := dht.ReadDHTxxWithRetry(sensorType, 4, true, 10)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// // Print temperature and humidity
-	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-	// 	temperature, humidity, retried)
-	// b, err := json.Marshal(models.WeatherMessage{
-	// 	Success:     "True",
-	// 	Message:     "Temperature and Humidity updated",
-	// 	Temperature: temperature,
-	// 	Humidity:    humidity,
-	// })
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// rw.Header().Set("Content-Type", "application/json")
-	// rw.Write(b)
-	// if temperature <= 22 {
-	// 	embd.SetDirection(lig.Pin_number, embd.Out)
-	// 	embd.DigitalWrite(lig.Pin_number, embd.High)
-	// 	b, err := json.Marshal(models.WeatherLEDMessage{
-	// 		Success: "True",
-	// 		Message: "It's getting cooler!!",
-	// 	})
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	rw.Header().Set("Content-Type", "application/json")
-	// 	rw.Write(b)
+	sensorType := dht.DHT11
+	temperature, humidity, retried, err := dht.ReadDHTxxWithRetry(sensorType, 4, true, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print temperature and humidity
+	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+		temperature, humidity, retried)
+	b, err := json.Marshal(models.WeatherMessage{
+		Success:     "True",
+		Message:     "Temperature and Humidity updated",
+		Temperature: temperature,
+		Humidity:    humidity,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(b)
+	if temperature <= 22 {
+		embd.SetDirection(lig.Pin_number, embd.Out)
+		embd.DigitalWrite(lig.Pin_number, embd.High)
+		b, err := json.Marshal(models.WeatherLEDMessage{
+			Success: "True",
+			Message: "It's getting cooler!!",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write(b)
 	// } else if temperature > 22 {
 	// 	embd.SetDirection(lig.Pin_number, embd.Out)
 	// 	embd.DigitalWrite(lig.Pin_number, embd.High)
